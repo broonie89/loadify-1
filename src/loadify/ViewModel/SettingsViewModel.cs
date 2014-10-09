4c56d973663ea6117b733ddd0914c97af22c4f0d
Mostey <mostey1337@hotmail.de>
2014-10-09 19:36:51 +0200

    Implemented settings

--- src/loadify/ViewModel/SettingsViewModel.cs
+++ src/loadify/ViewModel/SettingsViewModel.cs
@@ -3,12 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using loadify.Model;
 namespace loadify.ViewModel
 {
     public class SettingsViewModel : ViewModelBase
     {
-        public SettingsViewModel()
+        private SettingsModel _Settings;
+
+        public bool UseProxy
+        {
+            get { return _Settings.UseProxy; }
+            set
+            {
+                if (_Settings.UseProxy == value) return;
+                _Settings.UseProxy = value;
+                NotifyOfPropertyChange(() => UseProxy);
+            }
+        }
+
+        public string ProxyIP
+        {
+            get { return _Settings.ProxyIP; }
+            set
+            {
+                if (_Settings.ProxyIP == value) return;
+                _Settings.ProxyIP = value;
+                NotifyOfPropertyChange(() => ProxyIP);
+            }
+        }
+
+        public uint ProxyPort
+        {
+            get { return _Settings.ProxyPort; }
+            set
+            {
+                if (_Settings.ProxyPort == value) return;
+                _Settings.ProxyPort = value;
+                NotifyOfPropertyChange(() => ProxyPort);
+            }
+        }
+
+        public SettingsViewModel(SettingsModel settings)
+        {
+            _Settings = settings;
+        }
+
+        public SettingsViewModel():
+            this(new SettingsModel())
         { }
     }
 }
