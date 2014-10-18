5518bd21db207851fd0cbbc5b343efcfa7488f74
Mostey <mostey1337@hotmail.de>
2014-10-18 20:59:10 +0200

    Implemented AudioFileDescriptor and the Mp3FileDescriptor (structure only)

---
+++ src/loadify/Spotify/AudioFileMetaData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loadify.Spotify
+{
+    public class AudioFileMetaData
+    {
+        public string TrackName { get; set; }
+        public string ArtistName { get; set; }
+
+        public AudioFileMetaData(string trackName, string artistName)
+        {
+            TrackName = trackName;
+            ArtistName = artistName;
+        }
+    }
+}
---
+++ src/loadify/Spotify/Mp3FileDescriptor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loadify.Spotify
+{
+    public class Mp3FileDescriptor : AudioFileDescriptor
+    {
+        public Mp3FileDescriptor(AudioFileMetaData audioFileMetaData):
+            base(audioFileMetaData)
+        { }
+
+        public override void Write(string inputFilePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Read(string inputFilePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
