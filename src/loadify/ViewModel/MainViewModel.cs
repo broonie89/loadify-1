4dfcbe3bb508d1de2c23a14e1b95dc3af3e1f588
Mostey <mostey1337@hotmail.de>
2014-11-12 22:02:38 +0100

    Bugfix that prevents a nullreference exception from occuring when the input dialog for playlists is cancelled

--- src/loadify/ViewModel/MainViewModel.cs
+++ src/loadify/ViewModel/MainViewModel.cs
@@ -165,16 +165,16 @@ namespace loadify.ViewModel
         {
             var view = GetView() as MainView;
             var response = await view.ShowInputAsync("Add Playlist", "Please insert the link to the Spotify playlist you want to add.");
-            if (response.Length != 0)
+            if (!String.IsNullOrEmpty(response))
             {
                 var dialogResult =  await view.ShowMessageAsync("Add Playlist",
-                                                "Do you want to permanently add this playlist to your account?",
-                                                MessageDialogStyle.AffirmativeAndNegative,
-                                                new MetroDialogSettings()
-                                                {
-                                                    AffirmativeButtonText = "yes",
-                                                    NegativeButtonText = "no"
-                                                });
+                                                                "Do you want to permanently add this playlist to your account?",
+                                                                MessageDialogStyle.AffirmativeAndNegative,
+                                                                new MetroDialogSettings()
+                                                                {
+                                                                    AffirmativeButtonText = "yes",
+                                                                    NegativeButtonText = "no"
+                                                                });
 
                 _EventAggregator.PublishOnUIThread(new AddPlaylistReplyEvent(response, _Session, dialogResult == MessageDialogResult.Affirmative));
             }
