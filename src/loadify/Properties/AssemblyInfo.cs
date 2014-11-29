5841c231eb7713ad23dd3727e105e5ab1e4bb722
Mostey <mostey1337@hotmail.de>
2014-11-29 12:50:41 +0100

    Updated assembly version

--- src/loadify/Properties/AssemblyInfo.cs
+++ src/loadify/Properties/AssemblyInfo.cs
@@ -51,4 +51,4 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0 Nightly")]
+[assembly: AssemblyInformationalVersion("1.0 - Final")]
